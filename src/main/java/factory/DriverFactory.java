86eb32b13f2e1c5da265f0c981bd7e15a32b6f67
Urs Rust <urs.rust@solve.ch>
2025-09-29 12:47:11 +0200

    Small comment added

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
+// This is a simple example how we could move some setup code into an external factory
+// This factory creates the driver and adds our base configuration
+// but has no idea about the URL that we want to navigate to
 public class DriverFactory {
 
     public static WebDriver getNewChromeDriver() {
